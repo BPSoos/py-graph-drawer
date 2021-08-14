87c0ceea17cb5219ae606600afc49ee579a2a70f
BPSoos <soos.peter.96@gmail.com>
2021-08-15 00:20:23 +0200

    update run_graph-drawer

--- Libraries/yaml_parser.py
+++ Libraries/yaml_parser.py
@@ -4,8 +4,13 @@ import sys
 def get_value_from_file(file, value):
     with open(file, 'r') as f:
         parsed_file = yaml.load(f, Loader=yaml.FullLoader)
-        for key in parsed_file.keys():
-            print(parsed_file[key])
+        print(parsed_file[value])
+
+def get_graph_drawer_data(file, value):
+    with open(file, 'r') as f:
+        parsed_file = yaml.load(f, Loader=yaml.FullLoader)
+        print(parsed_file['graph-drawer'][value])
+
 
 
 if __name__ == "__main__":
--- docker/run_graph-drawer.bat
+++ docker/run_graph-drawer.bat
@@ -1,2 +1,9 @@
-set temp_dir=%cd%\..\runtemp
-docker run -td --name graph-drawer -v %temp_dir%:/root/runtemp -w /root/runtemp %1 bash
+SET temp_dir=%~dp0../runtemp/
+if [%1] NEQ [] (
+SET image=%1
+) else (
+python %temp_dir%../libraries/yaml_parser.py "get_graph_drawer_data('%temp_dir%../storage/versions_info.yaml','image')" > Output
+SET /p image=<Output
+DEL Output
+)
+ECHO docker run -td --name graph-drawer -v %temp_dir%:/root/runtemp -w /root/runtemp %image% bash
