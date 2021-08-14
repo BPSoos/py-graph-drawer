bd2929ebbbfdbfde9880ffe1f2cc257221731ae0
BPSoos <soos.peter.96@gmail.com>
2021-08-14 23:30:14 +0200

    update yaml_parser

--- Libraries/yaml_parser.py
+++ Libraries/yaml_parser.py
@@ -3,8 +3,9 @@ import sys
 
 def get_value_from_file(file, value):
     with open(file, 'r') as f:
-        data = yaml.parse(f.read())
-        print(str(data))
+        parsed_file = yaml.load(f, Loader=yaml.FullLoader)
+        for key in parsed_file.keys():
+            print(parsed_file[key])
 
 
 if __name__ == "__main__":
