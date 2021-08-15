fb4cb22b535433903b5d1013ec2dc94aa5f8cb46
BPSoos <soos.peter.96@gmail.com>
2021-08-15 15:27:26 +0200

    start to introduce pycairo

--- Libraries/CairoTester.py
+++ Libraries/CairoTester.py
@@ -10,27 +10,29 @@ ctx = Context(surface)
 ctx.scale(WIDTH, HEIGHT)
 
 pat = LinearGradient(0.0, 0.0, 0.0, 1.0)
-#pat.add_color_stop_rgba(1, 0.7, 0, 0, 0.5)
-#pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)
+pat.add_color_stop_rgba(0, 0.9, 0.7, 0.2, 1)
 
-ctx.rectangle(0, 0, 1, 1)
+ctx.rectangle(0, 0, 0.3, 0.3)
 ctx.set_source(pat)
 ctx.fill()
 
-ctx.translate(0.1, 0.1)
+#ctx.translate(0.1, 0.1)
 
-ctx.move_to(0, 0)
+print(ctx.get_current_point())
+#ctx.move_to(0, 0)
 
-ctx.arc(0.2, 0.1, 0.1, -math.pi / 2, 0)
+#ctx.arc(0.2, 0.1, 0.1, -math.pi / 2, 0)
 ctx.line_to(0.5, 0.1)
 
 ctx.curve_to(0.7, 0.5, 0.7, 0.7, 0.2, 0.8)
-ctx. close_path()
+ctx.close_path()
 
-ctx. close_path()
-
-ctx.set_source_rgb(0.0, 0.0, 0.0)
+ctx.set_source_rgb(0.1, 0.0, 0.0)
 ctx.set_line_width(0.02)
+ctx.stroke()
+
+ctx.move_to(0.4, 0.4)
+ctx.line_to(0.6, 0.8)
 ctx.stroke()
 
 if os.path.isfile(output):
--- Libraries/DrawTester.py
+++ Libraries/DrawTester.py
@@ -1,19 +1,29 @@
-import pygame
+import math
+import os
+from cairo import *
 
-from DrawHelper import *
-from BasicGraphObjects import *
+#from BasicGraphObjects import *
 
-pygame.init()
-pygame.font.init()
-my_font = pygame.font.Font(None, 30)
-pygame.display.set_caption('Your Graph')
 
-size = width, height = 1000, 900
-speed = [2, 2]
+WIDTH, HEIGHT = 1280, 720
+output = "/root/py-graph-drawer/runtemp/cairo_tester.png"
+surface = ImageSurface(FORMAT_ARGB32, WIDTH, HEIGHT)
+ctx = Context(surface)
+
+ctx.scale(WIDTH, HEIGHT)
+
+pat = LinearGradient(0.0, 0.0, 0.0, 1.0)
+pat.add_color_stop_rgba(0, 255/255, 255/255, 204/255, 1)
+
+ctx.rectangle(0, 0, 1, 1)
+ctx.set_source(pat)
+ctx.fill()
+ctx.set_source_rgb(0.1, 0.0, 0.0)
+ctx.set_line_width(0.001)
+screen = None
+
 background = (255, 255, 204)
-bg = BasicGraph(edges=[Edge(4, 2)])
-screen = pygame.display.set_mode(size)
-line_color = pygame.Color('black')
+#bg = BasicGraph(edges=[Edge(4, 2)])
 testing_points = {'start': [(200, 100), (100, 100), (100, 200), (100, 300),
                             (200, 300), (300, 300), (300, 200), (300, 100)],
                         'end': [(200, 200),(200, 200), (200, 200), (200, 200),
@@ -21,11 +31,14 @@ testing_points = {'start': [(200, 100), (100, 100), (100, 200), (100, 300),
 
 
 def draw_basic_line(start_point, end_point):
-    pygame.draw.line(screen, line_color, start_point, end_point, 2)
+    ctx.move_to(start_point[0]/WIDTH, start_point[1]/HEIGHT)
+    ctx.line_to(end_point[0]/WIDTH, end_point[1]/HEIGHT)
+    ctx.stroke()
 
 
 def draw_bounding_rect_for_segment(start_point, end_point):
-    pygame.draw.rect(screen, line_color, DrawHelper.get_rect_around_segment(start_point, end_point, get_top_rect=True), 1)
+    pass
+    #pygame.draw.rect(screen, line_color, DrawHelper.get_rect_around_segment(start_point, end_point, get_top_rect=True), 1)
 
 
 def draw_test_flower():
@@ -34,18 +47,16 @@ def draw_test_flower():
         start_point = tuple(cord * multiplier for cord in testing_points['start'][i])
         end_point = tuple(cord * multiplier for cord in testing_points['end'][i])
         draw_basic_line(start_point, end_point)
-        angle_1, angle_2, arc_rect = DrawHelper.get_arc_data_of_segment(start_point, end_point, False)
-        pygame.draw.arc(screen, pygame.Color('black'), arc_rect, angle_1, angle_2, 1)
-        angle_1, angle_2, arc_rect = DrawHelper.get_arc_data_of_segment(start_point, end_point, True)
-        pygame.draw.arc(screen, pygame.Color('black'), arc_rect, angle_1, angle_2, 1)
-        Vertice(*start_point).draw(screen)
-        Vertice(*end_point).draw(screen)
+        #angle_1, angle_2, arc_rect = DrawHelper.get_arc_data_of_segment(start_point, end_point, False)
+        #pygame.draw.arc(screen, pygame.Color('black'), arc_rect, angle_1, angle_2, 1)
+        #angle_1, angle_2, arc_rect = DrawHelper.get_arc_data_of_segment(start_point, end_point, True)
+        #pygame.draw.arc(screen, pygame.Color('black'), arc_rect, angle_1, angle_2, 1)
+        #Vertice(*start_point).draw(screen)
+        #Vertice(*end_point).draw(screen)
 
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        screen.fill(background)
-        draw_test_flower()
-        pygame.display.flip()
+draw_test_flower()
+
+if os.path.isfile(output):
+    os.remove(output)
+surface.write_to_png(output)
