1499d4e01507bcfefa736c03a875582a0e1da636
BPSoos <soos.peter.96@gmail.com>
2021-08-30 19:57:56 +0200

    added some basic layout functions

--- Libraries/BasicGraphObjects.py
+++ Libraries/BasicGraphObjects.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from DrawHelper import *
 PI = math.pi
@@ -15,7 +16,7 @@ class Vertice(Point, AbstractGraphObject):
     def __init__(self, x, y, context=None):
         super(Vertice, self).__init__(x, y)
         AbstractGraphObject.__init__(self, context)
-        self.radius = 15
+        self.radius = 0.15
 
     def draw(self):
         self.context.arc(self.x, self.y, self.radius, 0, 2*PI)
@@ -25,7 +26,7 @@ class Vertice(Point, AbstractGraphObject):
 
 class Edge(Segment, AbstractGraphObject):
 
-    def __init__(self, start_vertice, end_vertice, context=None):
+    def __init__(self, start_vertice, end_vertice, weight=None, context=None):
         super(Edge, self).__init__(start_vertice, end_vertice)
         AbstractGraphObject.__init__(self, context)
         self.edge_amount = 0
@@ -50,6 +51,9 @@ class BasicGraph(object):
         for vertices in vertice_list:
             self._edges.append(Edge(self.vertices[vertices[0]], self.vertices[vertices[1]]))
             
+    def set_vertice_radius(self, value):
+        for vertice in self.vertices:
+            vertice.radius = value
 
     @property
     def vertices(self):
@@ -77,16 +81,52 @@ class BasicGraph(object):
         self.vertices = []
         self.edges = []
         for i in range(0, len(adj)):
-            self.vertices.append(Vetice(0,0))
+            self.vertices.append(Vertice(0, 0))
         for i, vertice_edges in enumerate(adj):
             for edge in vertice_edges:
-                self.edges.append(Edge((self.vertices[i],self.vertices[edge[0]])))    
+                self.edges.append(Edge(self.vertices[i], self.vertices[edge[0]]))
 
     def create_layout_circle(self):
-        angle_interval = math.pi * 2 / len(self.vertices)
-        radius = 4
-        center = (5, 5)
-        
-        
-    
+        radius = 0.4
+        surf = self.vertices[0].context.get_target()
+        pix_height = surf.get_height()
+        pix_width = surf.get_width()
+        ctm_x = self.vertices[0].context.get_matrix()[0]
+        ctm_y = self.vertices[0].context.get_matrix()[3]
+        multiplier_x = pix_width/ctm_x
+        multiplier_y = pix_height/ctm_y
+        multiplier_main = multiplier_x if multiplier_y > multiplier_x else multiplier_y
+        print(multiplier_main, pix_width/ctm_x, pix_height/ctm_y)
+        self.set_vertice_radius(0.3*multiplier_main*(1/len(self.vertices)))
+        self.vertices[0].context.set_line_width(0.02*multiplier_main*(1/len(self.vertices)))
+        center_x = 0.5 * multiplier_x
+        center_y = 0.5 * multiplier_y
+
+        for i, vertice in enumerate(self.vertices):
+            gam = i * 2 * math.pi / len(self.vertices)
+            alf = math.pi - math.pi / 2 - (math.pi - gam) / 2
+            c = radius * 2 * math.sin(gam / 2)
+            dx = c * math.sin(alf) * multiplier_main
+            dy = c * math.cos(alf) * multiplier_main
+            x = center_x + radius * multiplier_main if dx >= 0 else center_x - radius * multiplier_main
+            y = center_y
+            vertice.x = (x - dx)
+            vertice.y = (y - dy)
+
+    def create_layout_random(self):
+        surf = self.vertices[0].context.get_target()
+        pix_height = surf.get_height()
+        pix_width = surf.get_width()
+        ctm_x = self.vertices[0].context.get_matrix()[0]
+        ctm_y = self.vertices[0].context.get_matrix()[3]
+        multiplier_x = pix_width/ctm_x
+        multiplier_y = pix_height/ctm_y
+        multiplier_main = multiplier_x if multiplier_y > multiplier_x else multiplier_y
+        print(multiplier_main, pix_width/ctm_x, pix_height/ctm_y)
+        self.set_vertice_radius(2*multiplier_main*(1/len(self.vertices)))
+        self.vertices[0].context.set_line_width(0.09*multiplier_main*(1/len(self.vertices)))
+
+        for i, vertice in enumerate(self.vertices):
+            vertice.x = random.randint(0, pix_width) / ctm_x
+            vertice.y = random.randint(0, pix_height) / ctm_y
 
--- Libraries/GraphTester.py
+++ Libraries/GraphTester.py
@@ -3,8 +3,8 @@ from cairo import *
 
 from BasicGraphObjects import *
 
-PIXEL_SCALE = 100
-WIDTH, HEIGHT = 12.80, 7.20
+PIXEL_SCALE = 2000
+WIDTH, HEIGHT = 10, 10
 output = "/root/py-graph-drawer/runtemp/cairo_tester.png"
 surface = ImageSurface(FORMAT_ARGB32, int(WIDTH*PIXEL_SCALE), int(HEIGHT*PIXEL_SCALE))
 ctx = Context(surface)
@@ -27,13 +27,35 @@ testing_points = {'start': [(200, 100), (100, 100), (100, 200), (100, 300),
 testing_adj = [[(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)],
                [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)],
                [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)], [(0, 1),(1, 1), (2, 1),(3, 1), (4, 1),(5, 1), (6, 1),(7, 1), (8, 1)]]
+with open("/root/py-graph-drawer/runtemp/road-minnesota.mtx", 'r') as f:
+    data = f.read().splitlines()
+testing_adj = []
+for i, line in enumerate(data):
+    testing_adj.append([])
+    for num in line.split(' '):
+        testing_adj[i].append((int(num), 0))
+print(testing_adj)
+
+'''with open("/root/py-graph-drawer/runtemp/email-Eu-core-temporal-Dept3.txt", 'r') as f:
+    data = f.read().splitlines()
+testing_adj = []
+for i in range(0,89):
+    testing_adj.append([])
+for i, line in enumerate(data):
+    line_data = line.split(' ')
+    testing_adj[int(line_data[0]) - 1].append((int(line_data[1]) - 1, int(line_data[2])))
+print(testing_adj)'''
+
 for pdict in testing_points.keys():
     for i, point in enumerate(testing_points[pdict]):
         testing_points[pdict][i] = Vertice(point[0]/PIXEL_SCALE, point[1]/PIXEL_SCALE, ctx)
-        testing_points[pdict][i].radius /= PIXEL_SCALE
+        #testing_points[pdict][i].radius /= PIXEL_SCALE
 print(testing_points)
 bg = BasicGraph(vertices=[*testing_points['start'],*testing_points['end']])
 bg.set_context(ctx)
+bg.init_adjacency(testing_adj)
+bg.set_context(ctx)
+bg.create_layout_random()
 for edge in bg.edges:
     edge.draw()
 for vertice in bg.vertices:
